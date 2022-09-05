a8dbc4ddb7a27cd4ee8aca31733ffab0e5f77297
anasmak04 <anasdev8@gmail.com>
2022-09-05 10:22:34 +0200

    fetch data

--- react-movies/src/components/Movies.jsx
+++ react-movies/src/components/Movies.jsx
@@ -1,32 +1,35 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 export default function Movies() {
-  const [query, setQuery] = useState("prison break");
+  const [query, setQuery] = useState("Elite");
   const [item, setItem] = useState([]);
+
   const options = {
     method: "GET",
-    url: "https://online-movie-database.p.rapidapi.com/title/find",
+    url: "https://imdb8.p.rapidapi.com/auto-complete",
     params: { q: `${query}` },
     headers: {
-      "X-RapidAPI-Key": "55904ea740msheb8e63c7e6c8ba0p11a9d5jsn06306b4199a4",
-      "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com",
+      "X-RapidAPI-Key": "c02a467fe7msh0c85f69b55746f4p1e11e9jsnd757a87b4be1",
+      "X-RapidAPI-Host": "imdb8.p.rapidapi.com",
     },
   };
 
   useEffect(() => {
-    axios.request(options)
-    .then((response) => {
-      setItem(response.data);
-       console.log(response.data);
-    })
+    axios
+      .request(options)
+      .then((response) => {
+        setItem(response.data);
+        console.log(response.data);
+      })
 
-    .catch((err) => {
-      console.log(err);
-    });
-  },[query])
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [query]);
   return (
     <div>
-      <h1>{item.query}</h1>
+      <h1>idd : {item.v}</h1>
+      <h1>Name : {item.q}</h1>
     </div>
   );
 }
